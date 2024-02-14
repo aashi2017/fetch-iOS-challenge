818a32b5e5a1bd1fde8e4f0e7b4e73927789911f
Aashi Shrimal <shrimalaashi07@gmail.com>
2024-02-14 01:23:27 -0500

    Initial Commit

---
+++ fetch-iOS-challenge/fetch_iOS_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_iOS_challengeApp.swift
+//  fetch-iOS-challenge
+//
+//  Created by AASHI  SHRIMAL on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_iOS_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
