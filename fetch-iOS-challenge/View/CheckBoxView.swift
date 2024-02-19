b86fd6954e4ad8ce3861f5b8f158fba8ff400267
Aashi Shrimal <shrimalaashi07@gmail.com>
2024-02-19 16:20:17 -0500

    image on details page

---
+++ fetch-iOS-challenge/View/CheckBoxView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckboxView.swift
+//  fetch-iOS-challenge
+//
+//  Created by AASHI  SHRIMAL on 2/19/24.
+//
+
+import SwiftUI
+
+struct CheckboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckboxView()
+}
